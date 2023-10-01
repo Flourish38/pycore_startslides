0e7c48d7cf4836c124d19b98d4107d8be2990a9f
Flourish38 <brsandvik@gmail.com>
2023-10-01 00:52:21 -0700

    include way of tracking best slide

--- startslide_random.py
+++ startslide_random.py
@@ -4,52 +4,74 @@ from Modules import mkw_classes as classes
 from Modules import mkw_core as core
 from Modules import mkw_translations as translate
 from Modules.framesequence import FrameSequence, Frame
+import os
+from copy import copy
 from random import getrandbits, randint
 
 
-def randomize_frame():
-    global currentFrame
+def randomize_current_frame():
+    global current_frame
 
-    currentFrame.accel = not getrandbits(1)
-    currentFrame.dpad_up = not getrandbits(1)
-    currentFrame.stick_x = randint(-7, 7)
-    currentFrame.stick_y = randint(-7, 7)
+    current_frame.accel = not getrandbits(1)
+    current_frame.dpad_up = not getrandbits(1)
+    current_frame.stick_x = randint(-7, 7)
+    current_frame.stick_y = randint(-7, 7)
 
 
 @event.on_frameadvance
 def onFrameAdvance():
-    global currentFrame, start_state
+    global start_state, best_completion, current_frame, frame_sequence
     stage = classes.RaceInfo.stage()
 
     if stage == 1:
         if start_state == None and core.get_frame_of_input() == 0:
             start_state = savestate.save_to_bytes()
 
-        randomize_frame()
-        TTK_Lib.writePlayerInputs(currentFrame)
+        randomize_current_frame()
+        TTK_Lib.writePlayerInputs(current_frame)
+        frame_sequence.frames.append(copy(current_frame))
 
     if stage == 2 and start_state != None:
         race_completion = classes.RaceInfoPlayer.race_completion()
-        gui.add_osd_message(f"Final Race Completion: {race_completion}")
+        gui.add_osd_message(f"Final race completion:   {race_completion}", 4000)
+        gui.add_osd_message(f"Current best completion: {best_completion}", 4000)
+
+        if race_completion > best_completion:
+            gui.add_osd_message(
+                f"New best completion by   {race_completion - best_completion}", 4000
+            )
+            best_completion = race_completion
+            frame_sequence.writeToFile(
+                os.path.join(
+                    utils.get_script_dir(),
+                    "MKW_Inputs",
+                    "Startslides",
+                    "best_random_inputs.csv",
+                )
+            )
+
+        frame_sequence = FrameSequence()
         savestate.load_from_bytes(start_state)
 
 
 def main() -> None:
-    global currentFrame, start_state
+    global start_state, best_completion, current_frame, frame_sequence
     start_state = None
-    currentFrame = Frame(["", "", "", "", "", ""])
+    best_completion = float("-inf")
+    current_frame = Frame(["", "", "", "", "", ""])
+    frame_sequence = FrameSequence()
 
-    currentFrame.accel = False
-    currentFrame.brake = False
-    currentFrame.item = False
-    currentFrame.stick_x = 0
-    currentFrame.stick_y = 0
-    currentFrame.dpad_up = False
-    currentFrame.dpad_down = False
-    currentFrame.dpad_left = False
-    currentFrame.dpad_right = False
-    currentFrame.valid = True
-    currentFrame.iter_idx = 0
+    current_frame.accel = False
+    current_frame.brake = False
+    current_frame.item = False
+    current_frame.stick_x = 0
+    current_frame.stick_y = 0
+    current_frame.dpad_up = False
+    current_frame.dpad_down = False
+    current_frame.dpad_left = False
+    current_frame.dpad_right = False
+    current_frame.valid = True
+    current_frame.iter_idx = 0
 
 
 if __name__ == "__main__":
