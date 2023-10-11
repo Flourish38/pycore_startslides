b9d5e39deaf307f1bbd29ac3a601e048885b4cc5
Flourish38 <brsandvik@gmail.com>
2023-10-10 18:27:17 -0700

    Always force full start boost

--- startslide_random.py
+++ startslide_random.py
@@ -9,10 +9,47 @@ from copy import copy
 from random import getrandbits, randint
 
 
-def randomize_current_frame(frame):
-    global current_frame
+def frames_up_to_full_boost(start_charge):
+    output = 0
+    while start_charge < 0.94:
+        output += 1
+        start_charge = 0.982 * start_charge + 0.02
+    return output
 
-    current_frame.accel = not getrandbits(1) if frame < 164 else True
+
+def frames_down_to_full_boost(start_charge):
+    output = 0
+    while start_charge > 0.95:
+        output += 1
+        start_charge *= 0.96
+    # There's a very good chance this will drop you out of range,
+    # so add on the number of frames to get back up in range.
+    return output + frames_up_to_full_boost(start_charge)
+
+
+def randomize_current_frame(frame):
+    global current_frame, holding_a
+
+    a_set = False
+    if holding_a or frame == 239:
+        current_frame.accel = True
+        a_set = True
+    # 136 is the first frame we might need to start holding A, since it takes 103 frames to get to full start boost from 0
+    elif frame >= 136:
+        charge = classes.KartState.start_boost_charge()
+        if charge < 0.94:
+            # Check to see if, if we DON'T push A, can we still get full start boost?
+            if frames_up_to_full_boost(0.96 * charge) + frame > 238:
+                current_frame.accel = True
+                holding_a = True
+                a_set = True
+        elif charge > 0.95 and frame >= 231:
+            # Check to see if, if we DO push A, can we still not burn out?
+            if frames_down_to_full_boost(min(1, 0.982 * charge + 0.02)) + frame > 238:
+                current_frame.accel = False
+                a_set = True
+    if not a_set:
+        current_frame.accel = not getrandbits(1)
     current_frame.dpad_up = not getrandbits(4)
     if frame == 0 or not getrandbits(3):
         current_frame.stick_x = randint(-7, 7)
@@ -21,7 +58,7 @@ def randomize_current_frame(frame):
 
 @event.on_frameadvance
 def onFrameAdvance():
-    global start_state, best_completion, current_frame, frame_sequence
+    global start_state, best_completion, current_frame, frame_sequence, holding_a
     stage = classes.RaceInfo.stage()
 
     # intro_timer == 171 is the frame before the start of input
@@ -38,12 +75,14 @@ def onFrameAdvance():
 
     if stage == 2 and start_state != None:
         race_completion = classes.RaceInfoPlayer.race_completion()
-        gui.add_osd_message(f"Final race completion:   {race_completion}", 4000)
-        gui.add_osd_message(f"Current best completion: {best_completion}", 4000)
+        gui.add_osd_message(
+            f"Final race completion:   {race_completion}\nCurrent best completion: {best_completion}",
+            3000,
+        )
 
         if race_completion > best_completion:
             gui.add_osd_message(
-                f"New best completion by   {race_completion - best_completion}", 4000
+                f"New best completion by   {race_completion - best_completion}", 3000
             )
             best_completion = race_completion
             frame_sequence.writeToFile(
@@ -55,16 +94,18 @@ def onFrameAdvance():
                 )
             )
 
+        holding_a = False
         frame_sequence = FrameSequence()
         savestate.load_from_bytes(start_state)
 
 
 def main() -> None:
-    global start_state, best_completion, current_frame, frame_sequence
+    global start_state, best_completion, current_frame, frame_sequence, holding_a
     start_state = None
     best_completion = float("-inf")
     current_frame = Frame(["", "", "", "", "", ""])
     frame_sequence = FrameSequence()
+    holding_a = False
 
     current_frame.accel = False
     current_frame.brake = False
